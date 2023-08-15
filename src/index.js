cd098f160de6d2ddea1951753ca743ea3df141fb
Thinh Phan <pseudothinhphan@gmail.com>
2023-08-14 17:36:11 -0700

    Add three buttons for nav bar

--- src/index.js
+++ src/index.js
@@ -2,8 +2,43 @@ import createMainPage from './main_page';
 import createContactPage from './contact_page';
 import createMenuPage from './menu_page';
 
+const navBar = document.getElementById('nav_bar');
 const primaryContent = document.getElementById('content');
 
+const homeBtn = document.createElement('button');
+homeBtn.setAttribute('id', 'home');
+homeBtn.textContent = 'Home';
+
+const menuBtn = document.createElement('button');
+menuBtn.setAttribute('id', 'menu');
+menuBtn.textContent = 'Menu';
+
+const contactBtn = document.createElement('button');
+contactBtn.setAttribute('id', 'contact');
+contactBtn.textContent = 'Contact';
+
+navBar.appendChild(homeBtn);
+navBar.appendChild(menuBtn);
+navBar.appendChild(contactBtn);
 primaryContent.appendChild(createMainPage());
-primaryContent.appendChild(createMenuPage());
-primaryContent.appendChild(createContactPage());
+
+const allBtns = document.querySelectorAll('button');
+
+allBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        switch(btn.id) {
+            case 'home':
+                primaryContent.innerHTML = '';
+                primaryContent.appendChild(createMainPage());
+                break;
+            case 'menu':
+                primaryContent.innerHTML = '';
+                primaryContent.appendChild(createMenuPage());
+                break;
+            case 'contact':
+                primaryContent.innerHTML = '';
+                primaryContent.appendChild(createContactPage());
+                break;
+        }
+    });
+});
