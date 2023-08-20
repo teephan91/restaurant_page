cfe244efe544cb4a435c72ccc6f0f6f9e0f90606
Thinh Phan <pseudothinhphan@gmail.com>
2023-08-20 15:38:14 -0700

    Separate nav_bar from website
    Create a separate JS file for creating the nav bar.

--- src/website.js
+++ src/website.js
@@ -1,63 +1,9 @@
-import createMainPage from './main_page';
-import createContactPage from './contact_page';
-import createMenuPage from './menu_page';
+import createNavBar from './nav_bar';
 import ghLogo from './github_logo.png';
 
 export default function createWebsite() {
+    createNavBar();
     const body = document.querySelector('body');
-    const navBar = document.getElementById('nav_bar');
-    const primaryContent = document.getElementById('content');
-
-    const homeBtn = document.createElement('button');
-    homeBtn.setAttribute('id', 'home');
-    homeBtn.textContent = 'Home';
-
-    const menuBtn = document.createElement('button');
-    menuBtn.setAttribute('id', 'menu');
-    menuBtn.textContent = 'Menu';
-
-    const contactBtn = document.createElement('button');
-    contactBtn.setAttribute('id', 'contact');
-    contactBtn.textContent = 'Contact';
-
-    navBar.appendChild(homeBtn);
-    navBar.appendChild(menuBtn);
-    navBar.appendChild(contactBtn);
-    primaryContent.appendChild(createMainPage());
-    homeBtn.classList.add('underlined_btn');
-
-    const allBtns = document.querySelectorAll('button');
-
-    allBtns.forEach(btn => btn.classList.add('nav_btn'));
-
-    allBtns.forEach(btn => {
-        btn.addEventListener('click', () => {
-            switch(btn.id) {
-                case 'home':
-                    menuBtn.classList.remove('underlined_btn');
-                    contactBtn.classList.remove('underlined_btn');
-                    primaryContent.innerHTML = '';
-                    primaryContent.appendChild(createMainPage());
-                    homeBtn.classList.add('underlined_btn');
-                    break;
-                case 'menu':
-                    homeBtn.classList.remove('underlined_btn');
-                    contactBtn.classList.remove('underlined_btn');
-                    primaryContent.innerHTML = '';
-                    primaryContent.appendChild(createMenuPage());
-                    menuBtn.classList.add('underlined_btn');
-                    break;
-                case 'contact':
-                    homeBtn.classList.remove('underlined_btn');
-                    menuBtn.classList.remove('underlined_btn');
-                    primaryContent.innerHTML = '';
-                    primaryContent.appendChild(createContactPage());
-                    contactBtn.classList.add('underlined_btn');
-                    break;
-            }
-        });
-    });
-
     const footer = document.createElement('footer');
     
     const copyright = document.createElement('p');
